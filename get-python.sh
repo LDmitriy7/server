26a9e6d6921d58fa62dce1658693b95048fcf6a8
levch <levchenko.d.a1998@gmail.com>
2022-05-14 07:18:55 +0300

    .

--- get-python.sh
+++ get-python.sh
@@ -3,8 +3,11 @@ apt upgrade
 
 apt install software-properties-common -y
 add-apt-repository ppa:deadsnakes/ppa
-apt install python3.10 python3.10-distutils python3-pip
+apt install python3.10 python3.10-distutils
+
+curl -sS https://bootstrap.pypa.io/get-pip.py | python
 
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
 update-alternatives --install /usr/bin/python python /usr/bin/python3.10 2
+update-alternatives --install /usr/bin/pip pip /usr/local/bin/pip 1
