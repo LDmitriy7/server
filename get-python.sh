5c4be5e2a39d950d856e43c34ef8c641706c8806
levch <levchenko.d.a1998@gmail.com>
2022-05-14 07:52:08 +0300

    .

--- get-python.sh
+++ get-python.sh
@@ -3,7 +3,7 @@ apt upgrade
 
 apt install software-properties-common
 add-apt-repository ppa:deadsnakes/ppa
-apt install python3.10 python3.10-distutils python3.10-apt
+apt install python3.10 python3.10-distutils python3-apt
 
 update-alternatives --install /usr/bin/python python /usr/bin/python3.10 2
 
